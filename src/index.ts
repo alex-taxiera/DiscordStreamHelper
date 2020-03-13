f631e4c0a003f2b4e5d0cfb51fbc744e1242b593
Alex Taxiera <alex.taxiera@gmail.com>
2020-03-12 23:30:22 -0400

    update env

--- src/index.ts
+++ src/index.ts
@@ -16,28 +16,28 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const {
-  STREAM_DISCORD_TOKEN,
-  STREAM_DB_CLIENT,
-  STREAM_DB_NAME,
-  STREAM_DB_USER,
-  STREAM_DB_PASS,
-  STREAM_DB_HOST,
-  STREAM_DB_CONNECTION
-} = (process.env as unknown) as ENV
+  DISCORD_TOKEN,
+  DB_CLIENT,
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+  DB_CONNECTION
+} = process.env as unknown as ENV
 
 /* create DataClient instance */
-const bot = new DataClient(STREAM_DISCORD_TOKEN, {
+const bot = new DataClient(DISCORD_TOKEN, {
   oratorOptions,
   statusManagerOptions,
   erisOptions: { disableEveryone: false },
   databaseManager: new SQLManager({
-    connectionInfo: STREAM_DB_CONNECTION || {
-      database: STREAM_DB_NAME,
-      user: STREAM_DB_USER,
-      password: STREAM_DB_PASS,
-      host: STREAM_DB_HOST
+    connectionInfo: DB_CONNECTION || {
+      database: DB_NAME,
+      user: DB_USER,
+      password: DB_PASS,
+      host: DB_HOST
     },
-    client: STREAM_DB_CLIENT,
+    client: DB_CLIENT,
     pool: {
       min: 0
     }
--- src/types/env.ts
+++ src/types/env.ts
@@ -1,9 +1,9 @@
 export interface ENV {
-  STREAM_DISCORD_TOKEN: string
-  STREAM_DB_CLIENT: string
-  STREAM_DB_NAME: string
-  STREAM_DB_USER: string
-  STREAM_DB_PASS: string
-  STREAM_DB_HOST: string
-  STREAM_DB_CONNECTION: string
+  DISCORD_TOKEN: string
+  DB_CLIENT: string
+  DB_NAME: string
+  DB_USER: string
+  DB_PASS: string
+  DB_HOST: string
+  DB_CONNECTION: string
 }
